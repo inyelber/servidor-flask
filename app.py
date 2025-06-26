e350e369b13079a3e4b59a2e5be3799d732fc210
inyelber <inyelber@gmail.com>
2025-06-25 20:40:42 -0400

    Primer commit

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hola desde Render!")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "🚀 API activa y lista para recibir datos del ESP"
+
+@app.route('/datos', methods=['POST'])
+def recibir_datos():
+    data = request.get_json()
+    print("📥 Datos recibidos:", data)
+    return jsonify({"status": "ok", "mensaje": "Datos recibidos correctamente"}), 200
+
+if __name__ == '__main__':
+    app.run()
